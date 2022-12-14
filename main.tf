d4fa2cc3ae4953c653d4b1cf1677d008f1ed38a6
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2022-12-13 21:34:02 -0500

    Add SES, CAA, MTA-STS and DNSSEC
    Signed-off-by: Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>

--- main.tf
+++ main.tf
@@ -46,3 +46,39 @@ module "acm_request_certificate" {
   subject_alternative_names         = [format("*.%s", aws_route53_zone.this.name)]
   context                           = module.this.context
 }
+
+module "caa" {
+  source  = "UGNS/route53-caa/aws"
+  version = "0.1.1"
+
+  zone_id              = aws_route53_zone.this.zone_id
+  caa_report_recipient = var.caa_report_recipient
+}
+
+module "ses" {
+  source  = "UGNS/route53-ses/aws"
+  version = "0.1.1"
+
+  zone_id = aws_route53_zone.this.zone_id
+}
+
+module "mta-sts" {
+  source  = "UGNS/route53-mta-sts/aws"
+  version = "0.1.1"
+
+  zone_id              = aws_route53_zone.this.zone_id
+  tls_report_recipient = var.tls_report_recipient
+  sts_policy_mode      = var.sts_policy_mode
+  mx_records           = var.mx_records
+}
+
+module "dnssec" {
+  source  = "UGNS/route53-dnssec/aws"
+  version = "1.1.0"
+
+  zones = {
+    this = {
+      zone_id = aws_route53_zone.this.zone_id
+    }
+  }
+}
